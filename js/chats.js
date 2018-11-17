4b6c33b32c4bb4d47b6546eb9508244019ced2a3
wang926454 <1107224733@qq.com>
2018-11-17 22:28:04 +0800

    区分地址留言，调整样式

--- js/chats.js
+++ js/chats.js
@@ -2,16 +2,18 @@ Bmob.initialize("c7401c529e15dad3f96901ac95e087dd", "8e9ecc1c5a2df4e02efe6a602f0
 
 var skip = 0;
 var size = 5;
+var url =  window.location.href;
 
 // 查询留言
 function queryChats(skip, size){
     const query = Bmob.Query("chats");
+    query.equalTo("url", "==", url);
     // 时间降序排列
     query.order("-createdAt");
     query.skip(skip);
     query.limit(size);
     query.find().then(res => {
-        console.log(res);
+        // console.log(res);
         var ul = document.getElementById("ul");
         ul.innerHTML = "";
         // 将查询的留言填写进ul li
@@ -31,6 +33,7 @@ function queryChats(skip, size){
         }
         // 留言为空
         if(res.length == 0){
+            layer.msg('还没有留言哦~');
             var li = document.createElement("li");
             li.innerHTML = "<label>还没有留言哦~</label>";
             ul.appendChild(li);
@@ -45,28 +48,32 @@ function addChats(){
     var web = document.getElementById("web").value;
     var msg = document.getElementById("msg").value;
     if(name.trim() === ''){
-        alert("昵称为空");
+        layer.msg('昵称为空');
         return;
     }
     if(msg.trim() === ''){
-        alert("内容为空");
+        layer.msg('内容为空');
         return;
     }
     if(name.indexOf("/script") != -1 || email.indexOf("/script") != -1 || web.indexOf("/script") != -1 || msg.indexOf("/script") != -1){
-        alert("请勿输入特殊字符");
+        layer.msg('请勿输入特殊字符');
         return;
     }
     const query = Bmob.Query('chats');
+    query.set("url", url);
     query.set("name", name);
     query.set("email", email);
     query.set("web", web);
     query.set("msg", msg);
+    
     query.save().then(res => {
-        console.log(res);
+        // console.log(res);
         reset();
+        layer.msg('留言成功');
         skip = 0;
         queryChats(skip, size);
     }).catch(err => {
+        layer.msg('留言失败，请联系管理员');
         console.log(err);
     })
 }
@@ -85,7 +92,7 @@ queryChats(skip, size);
 // 上一页
 function previous(){
     if(skip == 0){
-        alert("当前是第一页");
+        layer.msg('当前是第一页');
         return;
     }else{
         skip = skip - size;
@@ -97,16 +104,17 @@ function previous(){
 function next(){
     skip = skip + size;
     const query = Bmob.Query("chats");
+    query.equalTo("url", "==", url);
     // 时间降序排列
     query.order("-createdAt");
     query.skip(skip);
     query.limit(size);
     query.find().then(res => {
-        console.log(res);
+        // console.log(res);
         // 留言为空
         if(res.length == 0){
             skip = skip - size;
-            alert("当前是最后一页");
+            layer.msg('当前是最后一页');
             return;
         }else{
             var ul = document.getElementById("ul");
